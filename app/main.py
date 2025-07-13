5bffc6648958abe85e19cf77f45e364b5cb2b4a3
Dhruv <dhruv.gupta.cse17@iitbhu.ac.in>
2025-07-13 12:50:05 +0530

    bugfix

--- app/main.py
+++ app/main.py
@@ -287,7 +287,7 @@ def forward_and_get_answers(recvd_header_dict, received_questions, udp_socket, a
     response answer to the client.
 
     """
-    peer = ':'.split(address)
+    peer = tuple(':'.split(address))
     concat_answer = b''
 
     packet_id = recvd_header_dict["Packet ID"]
